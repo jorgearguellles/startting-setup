a9e97036a92cb5d70bf3e4d6d272b732806db6ad
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-06-06 17:53:49 -0500

    add/ value attribute on each input to have controled values. Clear each fields after submit event is called

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -52,10 +52,14 @@ const ExpenseForm = () => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(enteredDate);
+      date: new Date(enteredDate),
     };
 
     console.log({expenseData});
+    // To clear each field after click button
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -63,15 +67,15 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}/>
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
+          <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
+          <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className='new-expense__actions'>
